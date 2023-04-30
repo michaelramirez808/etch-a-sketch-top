158124c45ef260bd1964d9cbcac318590693a929
Michael <michael76ramirez@gmail.com>
2023-04-29 18:33:24 -0700

    16x16 grid outline w/ buttons--no functionality

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const container = document.createElement("div");
 container.id = "container";
-let isMouseDown = false;
+let isMouseDown = false
+let cells;
+container.style.gridTemplateColumns = "repeat(16, 1fr)";
 
-for (let i =1; i <=961 ; i++){
+
+for (let i =1; i <=256; i++){
     const div = document.createElement("div");
     div.classList.add("cell");
     container.appendChild(div);
+    
 
     div.addEventListener("mousedown", function(){
         isMouseDown = true;
@@ -27,3 +31,28 @@ for (let i =1; i <=961 ; i++){
 }
 
 document.body.appendChild(container);
+
+const grid16 = document.createElement("button");
+grid16.id = "grid16";
+grid16.classList.add("button")
+grid16.textContent = "16x16";
+document.body.appendChild(grid16);
+
+const grid32 = document.createElement("button");
+grid32.id = "grid32";
+grid32.classList.add("button")
+grid32.textContent = "32x32";
+document.body.appendChild(grid32);
+
+const grid64 = document.createElement("button");
+grid64.id = "grid64";
+grid64.classList.add("button")
+grid64.textContent = "64x64";
+document.body.appendChild(grid64);
+
+const buttonsDiv = document.createElement("div");
+buttonsDiv.classList.add("buttons-container");
+buttonsDiv.appendChild(grid16);
+buttonsDiv.appendChild(grid32);
+buttonsDiv.appendChild(grid64);
+document.body.appendChild(buttonsDiv);
